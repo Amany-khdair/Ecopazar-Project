149a825c24d81d84f48497cebad9269c26e23458
Amany-khdair <amany.khdair0@gmail.com>
2025-09-17 18:55:36 +0300

    Add modal using JS to the main page

--- assets/js/style.js
+++ assets/js/style.js
@@ -11,3 +11,71 @@ window.onscroll = function(e){
     }
 }
 
+const customModal = () => {
+    const myModal  = document.getElementById("staticBackdrop");
+    const images   = Array.from(document.querySelectorAll(".js_product_img"));
+    const thumbnails = Array.from(document.querySelectorAll(".js-img"));
+    const closeBtn = document.querySelector(".close-btn");
+    const eyeBtns  = Array.from(document.querySelectorAll(".showOn_modal"));
+    const upBtn    = document.querySelector(".btn-up");
+    const downBtn  = document.querySelector(".btn-down");
+    const modalImg = document.querySelector(".modal_img_js");
+
+    let currentIndex = 0;
+    let modalFlag = false;
+
+    const hideModal = () => {
+        myModal.classList.remove("show");
+        modalFlag = false;
+    }
+
+    const showModal = (index) => {
+        modalImg.src = images[index].src;
+        myModal.classList.add("show");
+        modalFlag = true;
+        currentIndex = index;
+    }
+
+    eyeBtns.forEach((btn, index) => {
+        btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        showModal(index);
+        updateImage(index);
+    });
+    });
+
+    closeBtn.addEventListener("click", hideModal);
+
+    document.addEventListener("click", (e) => {
+        if (modalFlag && e.target === myModal) hideModal();
+    });
+
+    
+    const updateImage = (newIndex) => {
+        if (newIndex < 0) newIndex = thumbnails.length - 1;
+        if (newIndex >= thumbnails.length) newIndex = 0;
+        currentIndex = newIndex;
+        modalImg.src = thumbnails[currentIndex].src;
+    }
+    thumbnails.forEach((thumb, index) => {
+        thumb.addEventListener("click", () => {
+            updateImage(index);
+        });
+    });
+    if (upBtn) upBtn.addEventListener("click", () => updateImage(currentIndex - 1));
+    if (downBtn) downBtn.addEventListener("click", () => updateImage(currentIndex + 1));
+
+    //keyboard events
+    document.addEventListener("keydown", ({code}) =>{
+        if (!myModal.classList.contains("show")) return; //will not work unless the modal is opened
+        if(code == 'ArrowUp' || code == 'ArrowRight'){
+            updateImage(currentIndex - 1);
+        }else if(code == 'ArrowDown' || code == 'ArrowLeft'){
+            updateImage(currentIndex + 1);
+        }else if(code == 'Escape'){
+            hideModal();
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded", customModal);
